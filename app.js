daef0fe422f68289a5ad5209dfd8716ee79e0d48
Earl Eskridge <EJay@Cindys-Air.lan>
2023-11-27 23:04:03 -0500

    completed todo number 1

--- app.js
+++ app.js
@@ -143,9 +143,17 @@ function findMexicanFood(allDishes) {
 
 function findItalianFood(allDishes) {
     alert("Searching for Italian dishes...")
+    let results = allDishes.filter(function (it) {
+        if (it.cuisine === "Italian") {
+            return true;
+        } else {
+            return false;
+        }
+    });
     // TODO #2: Filter all dishes for those that have a cuisine type of Italian
     alert("Found all Italian dishes!  Check the console for full output")
-}
+    return results 
+}  
 
 function searchCuisines(allDishes) {
     alert("Searching for dishes by cuisine...")
